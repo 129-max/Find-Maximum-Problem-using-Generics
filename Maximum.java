e6b62e2d60943bf690ee523be1b24b3b94adab1a
Mohit Raj <rajmohit.129@gmail.com>
2022-07-11 18:53:00 +0530

    [Mohit]Added:to check maximum three number

---
+++ Maximum.java
@@ -0,0 +1,32 @@
+package com.example.bridgelabz_genrics_problem;
+
+public class Maximum {
+
+    public static void findMaximum(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+        Integer maximumNumber;
+
+        if(firstNumber.compareTo(secondNumber)>0 && firstNumber.compareTo(thirdNumber)>0) {
+            maximumNumber = firstNumber;
+        }
+        else if(secondNumber.compareTo(firstNumber)>0 && secondNumber.compareTo(thirdNumber)>0) {
+            maximumNumber = secondNumber;
+        }
+        else {
+            maximumNumber = thirdNumber;
+        }
+
+        System.out.println("The maximum number is: " + maximumNumber);
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Generics");
+
+        Integer firstNumber = 15, secondNumber = 20, thirdNumber = 25;
+        findMaximum(firstNumber, secondNumber, thirdNumber);
+
+    }
+
+
+}
